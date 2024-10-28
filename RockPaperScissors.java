80f929ceca178665b9e6cda571c58df5c74d3120
DaveyAgni <devarchit.agnihotri@icloud.com>
2024-10-28 09:27:43 -0400

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -12,17 +12,78 @@ public class RockPaperScissors
 
 	public RockPaperScissors(String player)
 	{
+		int compRand = (int)((Math.random()*3)+1);
+		
+		if(compRand == 1)
+		{
+			compChoice = "R";
+		}
+		if(compRand == 2)
+		{
+			compChoice = "P";
+		}
+		if(compRand == 3)
+		{
+			compChoice = "S";
+		}
 	}
 
 	public String determineWinner()
 	{
-		String winner="";
-		return winner;
+		String winner = "";
+
+		if(playChoice == "R")
+		{
+			if(compChoice == "R")
+			{
+				winner = "tie"; 
+			}
+			if(compChoice == "S")
+			{
+				winner =  "computer wins";
+			}
+			if(compChoice == "P")
+			{
+				return "player wins";
+			}
+		}
+		if(playChoice == "S")
+		{
+			if(compChoice == "R")
+			{
+				winner =  "computer wins"; 
+			}
+			if(compChoice == "S")
+			{
+				winner =  "tie";
+			}
+			if(compChoice == "P")
+			{
+				winner =  "player wins";
+		}
+		if(playChoice == "P")
+		{
+			if(compChoice == "R")
+			{
+				winner = "computer wins"; 
+			}
+			if(compChoice == "S")
+			{
+				winner =  "player wins";
+			}
+			if(compChoice == "P")
+			{
+				winner =  "tie";
+			}
+		}
+
 	}
+		return winner;
+}
 
 	public String toString()
 	{
-		String output="";
+		String output=determineWinner();
 		return output;
 	}
 }
